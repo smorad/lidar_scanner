7671de46f26fdfdcce1d8cca0f3a2d9098cd63ab
Steven Morad <stevenmorad@gmail.com>
2017-11-08 20:43:03 -0700

    Begin work on planar handhold type

--- simulate/handhold_detectors.py
+++ simulate/handhold_detectors.py
@@ -1,6 +1,8 @@
 import networkx as nx
 import itertools
 import math
+import numpy
+from sklearn import linear_model, model_selection
 
 def euclidean_distance(a, b):
     return math.sqrt(
@@ -16,10 +18,44 @@ def find_local_maximum(graph, node):
         return node
     return None
 
+def assign_flatness(graph, node):
+    '''
+    Given a node, assigns it a score denoting how flat the immediate
+    area around the node is. The lower the score, the flatter it is, with
+    a score of 0 meaning the surface is a plane.
+    '''
+    #TODO search for all possible planes instead of x+y
+    # Generate normal of surface, align normal with Z axis
+    #https://stackoverflow.com/questions/1023948/rotate-normal-vector-onto-axis-plane
+    #https://math.stackexchange.com/questions/17246/is-there-a-way-to-rotate-the-graph-of-a-function
+    X = [(n.x, n.y) for n in graph.neighbors(node)]
+    t = [n.z for n in graph.neighbors(node)]
+    regression = linear_model.LinearRegression()
+    model = regression.fit(X, t)
+    #print('coef', regression.coef_)
+    
+    # find normal to plane
+    # since coef is just the int, ie 4 for 4x, it is already the deriv
+    # for a planar surface
+    model_gradient = (regression.coef_[0], regression.coef_[1], regression.coef_[0] + regression.coef_[1])
+    #print('model grad', model_gradient)
+
+    # What if instead of machine learning, we take every grad at every point and
+    # minimize xproduct, maybe compare it to ml'ed surface normal?
+    vectors = [(n.x, n.y, n.z) for n in graph.neighbors(node)]
+    point_gradients = numpy.gradient(vectors)
+    # fitness
+    # square for positive
+    #print(point_gradients[0])
+    # there are far too many things called "norm"
+    node.loss = sum([abs(numpy.linalg.norm(numpy.cross(p_grad, model_gradient))) for p_grad in point_gradients])
+    #print('loss', node.loss)
+
+
 class HandHoldGraph:
     def __init__(self, g: nx.Graph):
         # Dont modify original graph
-        self.g = g.copy()
+        self._g = g.copy()
 
     def get_graph(self):
         '''
@@ -27,20 +63,49 @@ class HandHoldGraph:
         '''
         raise NotImplementedError()
 
+    def _build_emst(self, nodes):
+        '''
+        Given a list of nodes, builds as complete euclidean graph.
+        Returns the euclidean minimum spanning tree of the graph.
+        '''
+        g = nx.Graph()
+        g.add_nodes_from(nodes)
+        # itertools does something weird here where it empties nodes, so deepcopy
+        all_possible_edges = list(itertools.permutations(nodes, 2))
+        [g.add_edge(*edge, weight=euclidean_distance(*edge)) 
+            for edge in all_possible_edges]
+        self.g = nx.minimum_spanning_tree(g)
+
+
     def get_path(self, start_node=None, goal_node=None):
         '''
         Return a path of nodes
         '''
         if not (start_node or goal_node):
-            for node in self.new_g.nodes:
+            for node in self.g.nodes:
                 if not goal_node:
                     goal_node = node
                     start_node = node
-                if node.x > goal_node.x and node.y > goal_node.y:
+                if node.y > goal_node.y:
                     goal_node = node
-                if node.x < start_node.x and node.y < start_node.y:
+                if node.y < goal_node.y:
                     start_node = node
-        return nx.shortest_path(self.new_g, start_node, goal_node)
+        return nx.shortest_path(self.g, start_node, goal_node)
+
+class Planar(HandHoldGraph):
+    '''For microspine grippers. Search for a flat, planar surface.'''
+
+    def get_graph(self, percentile=10):
+        '''Get emst with the flattest surfaces'''
+        assert(percentile > 0 and percentile < 100)
+
+        [assign_flatness(self._g, node)for node in self._g.nodes]
+        node_losses = sorted(self._g.nodes, key=lambda x: x.loss)
+        divider = int(percentile / 100 * len(node_losses))
+        nodes = node_losses[:divider + 1]
+        self._build_emst(nodes)
+        return self.g
+
 
 class Maxima(HandHoldGraph):
 
@@ -52,22 +117,13 @@ class Maxima(HandHoldGraph):
         # because 0 could potentially be a max
         maxima = filter(
             lambda x: x != None, 
-            [find_local_maximum(self.g, node) for node in self.g.nodes]
+            [find_local_maximum(self._g, node) for node in self._g.nodes]
         )
-
-        # Form a perfectly connected geometric graph, then remove 
-        # extra edges using euclidian MST, TODO make this more efficient
 
         # For some reason if we leave maxima as a filter object weird things 
         # happen, like the list being empty after iterating thru it
         maxima = list(maxima)
-        new_g = nx.Graph()
-        new_g.add_nodes_from(maxima)
-        # itertools does something weird here where it empties maxima, so deepcopy
-        all_possible_edges = list(itertools.permutations(maxima, 2))
-        [new_g.add_edge(*edge, weight=euclidean_distance(*edge)) 
-            for edge in all_possible_edges]
-        self.new_g = nx.minimum_spanning_tree(new_g)
-        return self.new_g
+        self._build_emst(maxima)
+        return self.g
 
 
--- simulate/simulate.py
+++ simulate/simulate.py
@@ -3,16 +3,14 @@ import graph_constructor
 import handhold_detectors
 
 g = graph_constructor.generate_graph('../data/mesh1.ply')
-h = handhold_detectors.Maxima(g)
+#h = handhold_detectors.Maxima(g)
+h = handhold_detectors.Planar(g)
 
 # Generate XYZ file for pics
 with open('../data/handholds.xyz', 'w') as f:
     for n in h.get_graph().nodes:
         f.write('{} {} {}\n'.format(int(n.x), int(n.y), int(n.z)))
     
-
-
-
 with open('../data/shortest_path.xyz', 'w') as f:
     for p in h.get_path():
         f.write('{} {} {}\n'.format(int(p.x), int(p.y), int(p.z)))
